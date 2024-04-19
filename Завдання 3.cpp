8f0f91ce601902eaf67493b4c11f945c520ee181
Vitaliy Pismak 723A <97937864+akiyamakai@users.noreply.github.com>
2024-04-19 15:47:03 +0300

    Add files via upload

---
+++ Завдання 3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int lengthOfString(const char* str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
+int main() {
+    const int MAX_LENGTH = 100;
+    char input[MAX_LENGTH];
+
+    std::cout << "Number 1: ";
+    std::cin.getline(input, MAX_LENGTH);
+
+    int length = lengthOfString(input);
+
+    std::cout << "Number 2: " << length << std::endl;
+
+    return 0;
+}
