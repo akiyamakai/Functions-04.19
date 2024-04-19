74ee7cc32805cfc762779530c0e9dc6ce963492c
akiyamakai <grusha.cs@gmail.com>
2024-04-19 15:36:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1, num2;
+
+    std::cout << "Number 1: ";
+    std::cin >> num1;
+
+    std::cout << "Number 2: ";
+    std::cin >> num2;
+
+    int result = sum(num1, num2);
+
+    std::cout << "Summa: " << result << std::endl;
+
+    return 0;
+}
