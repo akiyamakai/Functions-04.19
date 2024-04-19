f6f8e04d081667cc098ae841fc2b17508cd02a0c
Vitaliy Pismak 723A <97937864+akiyamakai@users.noreply.github.com>
2024-04-19 15:43:01 +0300

    Add files via upload

---
+++ Завдання 2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int num;
+
+    std::cout << "Write number: ";
+    std::cin >> num;
+
+    int result = factorial(num);
+
+    std::cout << "Factorian: " << num << " Summa: " << result << std::endl;
+
+    return 0;
+}
